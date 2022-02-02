a7f285eb1e3791d2e81784208754388960bf186b
Deebling5 <abhijay9@ymail.com>
2022-02-02 06:26:32 +0530

    first commit

---
+++ create_dataset.py
@@ -0,0 +1,29 @@
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+table_id = "eaglytics-340010.Assessment.assess1"
+
+job_config = bigquery.LoadJobConfig(
+    #schema=[
+    #    bigquery.SchemaField("name", "STRING"),
+    #    bigquery.SchemaField("post_abbr", "STRING"),
+    #],
+    autodetect=True,
+    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+)
+uri = "gs://eaglytics_assessment_abhijay/data_orders1"
+
+load_job = client.load_table_from_uri(
+    uri,
+    table_id,
+    location="US",  # Must match the destination dataset location.
+    job_config=job_config,
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)
+print("Loaded {} rows.".format(destination_table.num_rows))
---
+++ upload_to_gcs.py
@@ -0,0 +1,30 @@
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+
+bucket_name = 'eaglytics_assessment_abhijay'
+source_file_name = r'C:\Users\91801\OneDrive\Desktop\Testing\orders.json'
+destination_blob_name = 'data_orders1'
+
+upload_blob(bucket_name, source_file_name, destination_blob_name)
